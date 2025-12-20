606827ee6f453a2fa893ae25180fca104b4e612d
吉川普 <yoshikawahiroshi@yoshikawahiroshinoiMac.local>
2025-12-20 12:29:21 +0900

    build.shを修正

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python create_superuser.py  # 👈 これを追加！
+
+# ↓ここが新しい「最強の1行」です（管理者作成）
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin12345') if not User.objects.filter(username='admin').exists() else print('Admin already exists')" | python manage.py shell
