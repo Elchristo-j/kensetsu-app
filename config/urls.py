a465b6c98cf0aedecc1597510f1919294fa9471c
Elchristo-j <hiroshi.77dk@gmail.com>
2025-12-22 14:44:11 +0900

    Remove emergency code

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-
-def create_emergency_admin(request):
-    # ユーザー名 'admin' がいない場合のみ作成
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'AdminPass123!')
-        return HttpResponse("<h1>成功！</h1><p>管理者を作成しました。<br>ユーザー名: admin<br>パスワード: AdminPass123!</p>")
-    else:
-        return HttpResponse("<h1>確認</h1><p>管理者は既に存在しています。<br>パスワードが違う可能性があります。</p>")
 from django.conf import settings
 from django.conf.urls.static import static
 from jobs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('emergency-create/', create_emergency_admin),
     
     # トップページ（検索機能付き）
     path('', views.home, name='home'),
