84f11274c371b6a68cd35636d855740af755cf30
Elchristo-j <hiroshi.77dk@gmail.com>
2026-02-03 20:15:59 +0900

    fix url path

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from jobs import views as job_views  # 運営用のviewを呼び出すために追加
+from accounts import views as account_views # ★追加1：accountsのviewsを読み込む
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,6 +18,8 @@ urlpatterns = [
     path('operator/dashboard/', job_views.admin_dashboard, name='admin_dashboard'),
     path('operator/approve/<int:user_id>/', job_views.approve_profile, name='approve_profile'),
     path('operator/reject/<int:user_id>/', job_views.reject_profile, name='reject_profile'),
+
+    path('profile/', account_views.mypage, name='mypage'), # ★追加2：マイページへの通路
 ]
 
 # 画像表示設定
