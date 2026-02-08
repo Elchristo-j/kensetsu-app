10727d22a275ec2d3aa89f629b8b88ef64491e36
Elchristo-j <hiroshi.77dk@gmail.com>
2026-02-08 14:45:25 +0900

    Make migration 0010 empty to fix deploy error

--- accounts/migrations/0010_profile_is_founding_member_report_block.py
+++ accounts/migrations/0010_profile_is_founding_member_report_block.py
@@ -13,72 +13,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="profile",
-            name="is_founding_member",
-            field=models.BooleanField(default=False, verbose_name="創設メンバー"),
-        ),
-        migrations.CreateModel(
-            name="Report",
-            fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                ("reason", models.TextField(verbose_name="通報理由")),
-                ("created_at", models.DateTimeField(auto_now_add=True)),
-                (
-                    "reporter",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="reports_made",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-                (
-                    "target",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="reports_received",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-            ],
-        ),
-        migrations.CreateModel(
-            name="Block",
-            fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                ("created_at", models.DateTimeField(auto_now_add=True)),
-                (
-                    "blocked",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="blocked_by",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-                (
-                    "blocker",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="blocking",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
+        
             ],
             options={
                 "unique_together": {("blocker", "blocked")},
